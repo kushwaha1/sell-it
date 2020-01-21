abf9e1cb8775ee79ae6b1b6b307229503a070b18
ghanuisnotover <gs8111998@gmail.com>
2020-01-21 14:44:48 +0530

    AddBrand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MiddleComponent } from './middle/middle.component';
+import { AddBrandComponent } from './add-brand/add-brand.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MiddleComponent
+    MiddleComponent,
+    AddBrandComponent
   ],
   imports: [
     BrowserModule,
